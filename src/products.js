c47c8dff78c4d8058effaccf038ca6f6402ba2e6
JOHN NELSON <JohnNelson4850@gmail.com>
2019-07-22 17:11:25 -0700

    Product List created

---
+++ src/products.js
@@ -0,0 +1,55 @@
+
+const bomb = {
+    code: 'bomb',
+    name: 'Goblin Bomb',
+    image: 'assets.goblinBomb.jpg',
+    description: 'Great for problems Near and Far',
+    category: 'ranged',
+    price: 200,
+    cost: 50,
+};
+const grenade = {
+    code: 'grenade',
+    name: 'Goblin Grenade',
+    image: 'assets.goblinGrenade.jpg',
+    description: 'Great for increading personal space, pull pin and throw. . .',
+    category: 'ranged',
+    price: 50,
+    cost: 10,
+};
+const haveANiceDay = {
+    code: 'haveANiceDay',
+    name: 'Have a Nice Day',
+    image: 'assets.goblinHaveANiceDay.jpg',
+    description: 'end your day with a bang',
+    category: 'stationary',
+    price: 100,
+    cost: 25,
+};
+const mine = {
+    code: 'mine',
+    name: 'Goblin Mine',
+    image: 'assets.goblinMine.jpg',
+    description: 'Set it and DON\'T Forget it',
+    category: 'stationary',
+    price: 20,
+    cost: 5,
+};
+const tnt = {
+    code: 'tnt',
+    name: 'Goblin TNT',
+    image: 'assets.goblinTNT.jpg',
+    description: 'Simple yet Effective',
+    category: 'stationary',
+    price: 10,
+    cost: 2,
+};
+const largetnt = {
+    code: 'largetnt',
+    name: 'Goblin TNT, Large',
+    image: 'assets.goblinLargeTNT.jpg',
+    description: 'Bigger is Better',
+    category: 'stationary',
+    price: 75,
+    cost: 20,
+};
